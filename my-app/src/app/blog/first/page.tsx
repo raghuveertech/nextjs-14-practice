7baa2a4e77e428018cb5c33512507dd436409427
raghuveertech <raghuveerp25@gmail.com>
2023-12-29 19:09:42 +0530

    nested routes

---
+++ my-app/src/app/blog/first/page.tsx
@@ -0,0 +1,9 @@
+const FirstPost = () => {
+  return (
+    <div>
+      <h1>Blog Post 1!</h1>
+    </div>
+  );
+};
+
+export default FirstPost;
---
+++ my-app/src/app/blog/second/page.tsx
@@ -0,0 +1,9 @@
+const SecondPost = () => {
+  return (
+    <div>
+      <h1>Blog Post 2!</h1>
+    </div>
+  );
+};
+
+export default SecondPost;
