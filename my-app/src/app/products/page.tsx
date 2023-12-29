adb771f04393b02e9c2a8a933ebe35d8f7750ef8
raghuveertech <raghuveerp25@gmail.com>
2023-12-29 20:00:10 +0530

    dynamic routes

---
+++ my-app/src/app/products/page.tsx
@@ -0,0 +1,12 @@
+const ProductList = () => {
+  return (
+    <>
+      <h1>Produsts List</h1>
+      <h2>Product 1</h2>
+      <h2>Product 2</h2>
+      <h2>Product 3</h2>
+    </>
+  );
+};
+
+export default ProductList;
